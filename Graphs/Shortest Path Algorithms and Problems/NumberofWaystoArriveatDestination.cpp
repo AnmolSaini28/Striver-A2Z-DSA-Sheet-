48d6f9abe7f9b36c861c8eb0ee92b51b8d0fba3e
Anmol Saini <143955945+AnmolSaini28@users.noreply.github.com>
2024-06-26 23:13:31 +0530

    Create Number of Ways to Arrive at Destination

---
+++ Graphs/Shortest Path Algorithms and Problems/NumberofWaystoArriveatDestination.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        vector<pair<int, int>> adj[n];
+        for (auto it : roads)
+        {
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+        priority_queue<pair<long long, long long>,
+                       vector<pair<long long, long long>>, greater<pair<long long, long long>>> pq;
+
+        vector<long long> dist(n, INT_MAX);
+        vector <int> ways(n, 0);
+        dist[0] = 0;
+        ways[0] = 1;
+        pq.push({0, 0});
+
+        int mod = (int)(1e9 + 7);
+
+        while (!pq.empty())
+        {
+            long long dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            for (auto it : adj[node])
+            {
+                int adjNode = it.first;
+                long long edW = it.second;
+
+                if (dis + edW < dist[adjNode])
+                {
+                    dist[adjNode] = dis + edW;
+                    pq.push({dis + edW, adjNode});
+                    ways[adjNode] = ways[node];
+                }
+
+                else if (dis + edW == dist[adjNode])
+                {
+                    ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
+                }
+            }
+        }
+        
+        return ways[n - 1] % mod;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n, m;
+        cin >> n >> m;
+        int u, v;
+
+        vector<vector<int>> adj;
+
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 3; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+
+        Solution obj;
+        cout << obj.countPaths(n, adj) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
