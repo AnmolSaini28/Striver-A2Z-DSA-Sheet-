2c4cbf92a1d215c03af4766fa4b7c0584aa63411
Anmol Saini <143955945+AnmolSaini28@users.noreply.github.com>
2024-07-08 12:11:19 +0530

    Create Frog Jump with K distances

---
+++ Dynamic Programming [Patterns and Problems]/1D DP/FrogJumpwithKdistances.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int minimizeCost(vector<int>& height, int n, int k) {
+        vector <int> dp(n);
+        dp[0] = 0;
+        for(int i=1 ; i<n ; i++){
+            int minJumps = INT_MAX;
+            for(int j=1 ; j<=k ; j++){
+                int jumps;
+                if((i-j) >= 0){
+                    jumps = dp[i-j] + abs(height[i]-height[i-j]);
+                }
+                minJumps = min(minJumps,jumps);
+            }
+            dp[i] = minJumps;
+        }
+        return dp[n-1];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, K;
+        cin >> N >> K;
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.minimizeCost(arr, N, K) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
