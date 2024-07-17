62ea1c0085ad033d0f16977fcf9db7312991ff09
Anmol Saini <143955945+AnmolSaini28@users.noreply.github.com>
2024-07-17 18:37:20 +0530

    Create Subset sum equal to target

---
+++ Dynamic Programming [Patterns and Problems]/DPonSubsequences/Subsetsumequaltotarget.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    bool isSubsetSum(vector<int>arr, int k){
+        int n = arr.size();
+        vector <vector <bool>> dp(n, vector <bool>(k+1, false));
+        for(int i=0 ; i<n ; i++){
+            dp[i][0] = true;
+        }
+        if(arr[0] <= k){
+            dp[0][arr[0]] = true;
+        }
+        for(int ind=1 ; ind<n ; ind++){
+            for(int target=1 ; target<=k ; target++){
+                bool notTaken = dp[ind-1][target];
+                bool Taken = false;
+                if(arr[ind] <= target){
+                    Taken = dp[ind-1][target-arr[ind]];
+                }
+                dp[ind][target] = notTaken || Taken;
+            }
+        }
+        return dp[n-1][k];
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, sum;
+        cin >> N;
+        vector<int> arr(N);
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+        cin >> sum;
+        
+        Solution ob;
+        cout << ob.isSubsetSum(arr, sum) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
